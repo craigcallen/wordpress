d52e5a9787d8b02c11337b163c5586e4e01a4762
Craig Allen <me@craigcallen.com>
2023-02-01 15:29:36 -0600

    Delete acf-google-maps-api.php

--- acf-google-maps-api.php
+++
@@ -1,7 +0,0 @@
-<?
-//Add Google Maps API to Advanced Custom Fields
-function my_acf_google_map_api( $api ){
-    $api['key'] = 'Key Goes Here';
-    return $api;
-}
-add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
