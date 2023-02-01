91a4151a8bab098e65438420108c02732bfc5c0e
Craig Allen <me@craigcallen.com>
2023-02-01 15:27:52 -0600

    Update acf-google-maps-api.php

--- acf-google-maps-api.php
+++ acf-google-maps-api.php
@@ -1,7 +1,7 @@
 <?
 //Add Google Maps API to Advanced Custom Fields
 function my_acf_google_map_api( $api ){
-    $api['key'] = 'AIzaSyAo7J8dUFkSR6Dscs-JHRkEsOV-sJU_CP8';
+    $api['key'] = 'Key Goes Here';
     return $api;
 }
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
